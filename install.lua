ae708420d4cd21fe18e5e68950c945980540e5bf
Cooper <coop@coopeeo.xyz>
2025-07-18 18:07:11 -0500

    lets see if this works?

--- install.lua
+++ install.lua
@@ -13,7 +13,7 @@ while true do
     elseif key == keys.n then
         term.setCursorPos(cursorPosX + 1, 0)
         term.write("Installation canceled.")
-        return
+        exit()
     end
 end
 term.write("testing")
